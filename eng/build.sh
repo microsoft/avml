6680d4e427c8f0f1fff3225d35927adb440513cc
Brian Caswell <bmc@shmoo.com>
2022-02-09 14:19:44 -0500

    move from ADO to GitHub Actions

--- eng/build.sh
+++ eng/build.sh
@@ -9,10 +9,11 @@ rustup component add rustfmt
 rustup target add x86_64-unknown-linux-musl
 
 cargo fmt -- --check
-cargo clippy -- -D clippy::pedantic -A clippy::missing_errors_doc
 cargo build --release --no-default-features --target x86_64-unknown-linux-musl --locked
 cp target/x86_64-unknown-linux-musl/release/avml target/x86_64-unknown-linux-musl/release/avml-minimal
 cargo build --release --target x86_64-unknown-linux-musl --locked
+cargo test --release --target x86_64-unknown-linux-musl --locked
+cargo clippy -- -D clippy::pedantic -A clippy::missing_errors_doc
 strip target/x86_64-unknown-linux-musl/release/avml
 strip target/x86_64-unknown-linux-musl/release/avml-minimal
 strip target/x86_64-unknown-linux-musl/release/avml-convert
