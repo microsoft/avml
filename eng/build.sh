94fe4fa3e0ba291a2796762b2e7b0c53739fd994
Brian Caswell <bmc@shmoo.com>
2025-02-06 12:18:42 -0500

    add support for arm64 builds

--- eng/build.sh
+++ eng/build.sh
@@ -8,20 +8,22 @@ set -uvex -o pipefail
 
 cd $(dirname ${BASH_SOURCE[0]})/../
 
+ARCH=$(uname -m)
+
 typos
 
 cargo fmt -- --check
 cargo semver-checks check-release
 cargo clippy --locked --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc
-cargo test --release --target x86_64-unknown-linux-musl --locked --all-targets --all-features
+cargo test --release --target ${ARCH}-unknown-linux-musl --locked --all-targets --all-features
 for FEATURE in $(cargo metadata --locked --format-version 1 | jq '.packages | [.[] | select(.name=="avml")][0].features | keys | @tsv' -r); do
-    cargo check --release --target x86_64-unknown-linux-musl --locked --no-default-features --features ${FEATURE} --features native-tls
+    cargo check --release --target ${ARCH}-unknown-linux-musl --locked --no-default-features --features ${FEATURE} --features native-tls
 done
-cargo build --release --no-default-features --target x86_64-unknown-linux-musl --locked
-cp target/x86_64-unknown-linux-musl/release/avml target/x86_64-unknown-linux-musl/release/avml-minimal
-cargo build --release --target x86_64-unknown-linux-musl --locked
-cargo build --release --target x86_64-unknown-linux-musl --locked --bin avml-upload --features "put blobstore status"
-strip target/x86_64-unknown-linux-musl/release/avml
-strip target/x86_64-unknown-linux-musl/release/avml-minimal
-strip target/x86_64-unknown-linux-musl/release/avml-convert
-strip target/x86_64-unknown-linux-musl/release/avml-upload
+cargo build --release --no-default-features --target ${ARCH}-unknown-linux-musl --locked
+cp target/${ARCH}-unknown-linux-musl/release/avml target/${ARCH}-unknown-linux-musl/release/avml-minimal
+cargo build --release --target ${ARCH}-unknown-linux-musl --locked
+cargo build --release --target ${ARCH}-unknown-linux-musl --locked --bin avml-upload --features "put blobstore status"
+strip target/${ARCH}-unknown-linux-musl/release/avml
+strip target/${ARCH}-unknown-linux-musl/release/avml-minimal
+strip target/${ARCH}-unknown-linux-musl/release/avml-convert
+strip target/${ARCH}-unknown-linux-musl/release/avml-upload
