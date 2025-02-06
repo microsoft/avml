5e8e85d4997bc2deeaf13247679ef40a6d19717f
Brian Caswell <bmc@shmoo.com>
2025-02-06 13:27:08 -0500

    add support for arm64 builds (#615)

--- eng/setup.sh
+++ eng/setup.sh
@@ -9,7 +9,7 @@ set -uvex -o pipefail
 DEBIAN_FRONTEND=noninteractive sudo apt-get install musl-dev musl-tools musl jq
 
 rustup component add rustfmt
-rustup target add x86_64-unknown-linux-musl
+rustup target add $(uname -m)-unknown-linux-musl
 rustup update stable
 cargo install typos-cli
 cargo install cargo-semver-checks --locked
