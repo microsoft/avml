cd0f0ffe1705443b9a8fdc53504c5b7e371fc46e
Brian Caswell <bmc@shmoo.com>
2025-03-13 11:08:04 -0700

    address if_then_some_else_none lint (#635)

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 #![deny(clippy::panic)]
 #![deny(clippy::manual_assert)]
 #![deny(clippy::indexing_slicing)]
+#![deny(clippy::if_then_some_else_none)]
 #![deny(clippy::shadow_unrelated)]
 #![deny(clippy::std_instead_of_core)]
 
