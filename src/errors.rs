c1d9913f84d07bce47af2c97c0a25774668a244d
Brian Caswell <bmc@shmoo.com>
2025-03-24 11:13:17 -0400

    move crate::Error to crate::errors::Error

---
+++ src/errors.rs
@@ -0,0 +1,61 @@
+use core::{
+    error::Error as StdError,
+    fmt::{Debug as FmtDebug, Formatter, Result as FmtResult},
+};
+#[cfg(any(feature = "blobstore", feature = "put"))]
+use std::io::Error as IoError;
+
+#[derive(thiserror::Error)]
+pub enum Error {
+    #[error("unable to create snapshot")]
+    Image(#[from] crate::image::Error),
+
+    #[error("unable to read memory")]
+    Memory(#[from] crate::snapshot::Error),
+
+    #[error("unable to parse /proc/iomem")]
+    Iomem(#[from] crate::iomem::Error),
+
+    #[cfg(feature = "put")]
+    #[error("unable to upload file via PUT")]
+    Upload(#[from] crate::upload::http::Error),
+
+    #[cfg(feature = "blobstore")]
+    #[error("unable to upload file to Azure Storage")]
+    Blob(#[from] crate::upload::blobstore::Error),
+
+    #[cfg(any(feature = "blobstore", feature = "put"))]
+    #[error("tokio runtime error: {0}")]
+    Tokio(#[source] IoError),
+
+    #[cfg(any(feature = "blobstore", feature = "put"))]
+    #[error("unable to remove snapshot")]
+    RemoveSnapshot(#[source] IoError),
+
+    #[error("no conversion required")]
+    NoConversionRequired,
+}
+
+pub(crate) fn format_error(e: &impl StdError, f: &mut Formatter) -> FmtResult {
+    write!(f, "error: {e}")?;
+
+    let mut source = e.source();
+
+    if e.source().is_some() {
+        writeln!(f, "\ncaused by:")?;
+        let mut i: usize = 0;
+        while let Some(inner) = source {
+            writeln!(f, "{i: >5}: {inner}")?;
+            source = inner.source();
+            i = i.saturating_add(1);
+        }
+    }
+
+    Ok(())
+}
+
+impl FmtDebug for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        format_error(self, f)
+    }
+}
